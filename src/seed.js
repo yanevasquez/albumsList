f42b1cfd0d1d451bf9d4b568a216421a29850c81
yane <yanelisset4@gmail.com>
2020-05-02 21:00:57 -0300

    > seed.js with array of objects: albumslist

---
+++ src/seed.js
@@ -0,0 +1,66 @@
+export const albumslist = [{
+        id: 1,
+        artist: 'Moptop',
+        description: 'mopTOP',
+        url: '#',
+        votes: 22,
+        cover: require('./assets/images/submissions/moptop.jpg')
+    },
+    {
+        id: 2,
+        artist: 'Selvagens a procura de lei',
+        description: 'essa capa é um poster',
+        url: '#',
+        votes: 15,
+        cover: require('./assets/images/submissions/sapdl.jpg')
+    },
+    {
+        id: 3,
+        artist: 'Dônica',
+        description: 'tranquilo',
+        url: '#',
+        votes: 14,
+        cover: require('./assets/images/submissions/donica.png')
+    },
+    {
+        id: 4,
+        artist: 'Balthazar',
+        description: 'indie massa',
+        url: '#',
+        votes: 17,
+        cover: require('./assets/images/submissions/balthazar.jpg')
+    },
+    {
+        id: 5,
+        artist: 'The Marías',
+        description: 'procurei um defeito e não achei',
+        url: '#',
+        votes: 16,
+        cover: require('./assets/images/submissions/themarias1.jpg')
+    },
+    {
+        id: 6,
+        artist: 'Madonna',
+        description: 'rainha, queridos',
+        url: '#',
+        votes: 23,
+        cover: require('./assets/images/submissions/madonna.jpg')
+    },
+    {
+        id: 7,
+        artist: 'Carne Doce',
+        description: 'to morando nesse album desde que conheci',
+        url: '#',
+        votes: 23,
+        cover: require('./assets/images/submissions/carnedoce.jpeg')
+    },
+    {
+        id: 8,
+        artist: 'O terno',
+        description: 'acolhedor',
+        url: '#',
+        votes: 23,
+        cover: require('./assets/images/submissions/atrasealem.jpg')
+    }
+];
+a
